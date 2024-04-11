5861f6b25448b55ce85d806505b220519fe438b0
dericholas <nickderis@gmail.com>
2024-04-11 13:00:29 -0400

    light refactor

--- server/src/serializers/FollowSerializer.js
+++ server/src/serializers/FollowSerializer.js
@@ -1,9 +1,6 @@
 import { Follow } from "../models/index.js"
-
 class FollowSerializer {
     static async getFollowDetails(userObject, currentUserId) {
-        console.log("userObjectFromSerializer", userObject)
-        console.log("currentUserId", currentUserId)
 
         const allowedAttributes = ["id", "username", "createdAt"]
         const serializedFollow = {}
@@ -13,17 +10,11 @@ class FollowSerializer {
         }
         const followingRelation = await Follow.query().where({followingUserId: currentUserId, followedUserId: userObject.id})
         const followedRelation = await Follow.query().where({followingUserId: userObject.id, followedUserId: currentUserId})
-        console.log("followedRelation", followedRelation)
-        console.log("followingRelation", followingRelation)
 
         serializedFollow.isFollowing = followingRelation.length > 0 ? true : false
         serializedFollow.isFollowed = followedRelation.length > 0 ? true : false
-        console.log("ISFOLLOWING", serializedFollow.isFollowing)
-        console.log("ISFOLLOWED", serializedFollow.isFollowed)
-        // const isFollowing = !!(await Follow.query().where({followingUserId: currentUserId, followedUserId: userObject.id}))
 
         return serializedFollow
     }
 }
 export default FollowSerializer
-
